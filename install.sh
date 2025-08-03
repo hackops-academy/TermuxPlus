a1202ffb720d0c5a37a6d11a01d02c8e8fddda2d
Rajput Lekhraj Singh <hackops59@gmail.com>
2025-08-03 11:24:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
 
+clear
+echo -e "\e[1;92m[+] Installing TermuxPlus Environment...\e[0m"
+
+# Update & Install Essentials
+pkg update -y && pkg upgrade -y
+pkg install -y zsh git curl wget figlet toilet lolcat python nano
+
+# Install Oh-My-Zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "[+] Installing Oh-My-Zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Copy custom zsh config
+cp .zshrc-template ~/.zshrc
+
+# Add Aliases
+cat aliases.sh >> ~/.zshrc
+
+# Set up Banner
+echo "[+] Setting up banner..."
+echo "bash ~/TermuxPlus/banner.sh" >> ~/.zshrc
+
+# Install Fonts (optional)
+pkg install -y nerd-fonts
+
+# Default Shell = Zsh
+chsh -s zsh
+
+echo -e "\n[✔] Installation Complete! Restart Termux or type \e[1;96mzsh\e[0m to activate TermuxPlus."
