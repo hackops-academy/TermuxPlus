b10a37f151e73c5323d0ef385668b2af58bfc885
Rajput Lekhraj Singh <hackops59@gmail.com>
2025-08-03 11:25:52 +0530

    Update tools-installer.sh

--- tools-installer.sh
+++ tools-installer.sh
@@ -1,1 +1,22 @@
-
+#!/bin/bash
+echo -e "\n\e[1;96m[+] TermuxPlus Tool Installer Menu\e[0m"
+PS3="Select a tool to install: "
+options=("Nmap" "Sqlmap" "Hydra" "Exit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Nmap")
+            pkg install -y nmap
+            ;;
+        "Sqlmap")
+            git clone https://github.com/sqlmapproject/sqlmap
+            ;;
+        "Hydra")
+            pkg install -y hydra
+            ;;
+        "Exit")
+            break
+            ;;
+        *) echo "Invalid option" ;;
+    esac
+done
