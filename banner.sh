8a41d6cf00071d5512646d54ff529d307603f4bc
Rajput Lekhraj Singh <hackops59@gmail.com>
2025-08-03 11:25:03 +0530

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+clear
+figlet "TermuxPlus" | lolcat
+echo -e "\nWelcome back, $USER!" | lolcat
+echo -e "Date: $(date)" | lolcat
+echo -e "IP: $(curl -s ifconfig.me)" | lolcat
+termux-battery-status | jq '.percentage' | awk '{print "Battery: " $1"%"}' | lolcat
+df -h | grep /data | awk '{print "Storage: "$4" Free"}' | lolcat
+echo ""
